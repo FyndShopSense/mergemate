a76fd5ef641acb18d547368b3083152894c12aac
fex <fex@gofynd.com>
2024-11-25 16:41:43 +0530

    fex-commit: Fix applied based on fex review suggestions

--- app/db/test.py
+++ app/db/test.py
@@ -8,7 +8,7 @@ print(trimmed_lines.index('app = FastAPI()'))
 def is_prime(n):
     if n <= 1:
         return False
-    for i in range(2, n):
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
